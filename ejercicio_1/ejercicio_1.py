d6c7bdcfe5859b7d8ad89d587603bedd122ddc9d
Juanherrera117 <relujuca@gmail.com>
2022-05-24 15:48:39 -0500

    Ejercicios_while

---
+++ ejercicio_1/ejercicio_1.py
@@ -0,0 +1,19 @@
+"""CANTIDAD DE VECES QUE SALE EL NUMERO 3 AL LANZAR N VECES UN DADO"""
+
+print("-------------------")
+print("------N VECES------")
+print("-------------------")
+
+import random
+I = 1
+N = 0
+#processing
+while I <= 10:
+    numero = random.randint(1,6)
+    print(str(numero))
+    I = I + 1 
+    if numero / 3 == 1:
+        N = N + 1
+        
+
+print("Las veces que sale el numero 3 es : " + str(N))
